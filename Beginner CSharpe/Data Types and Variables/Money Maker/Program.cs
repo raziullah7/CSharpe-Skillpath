fd995be9d59b58525b2c5b2fcaf412f077806ca9
Razi Ullah <raziullah913@gmail.com>
2023-02-07 01:05:22 +0500

    program to convert cents to gold, silver and bronze coins

---
+++ Beginner CSharpe/Data Types and Variables/Money Maker/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace MoneyMaker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // A bronze coin is worth 1 cent
+            // A silver coin is worth 5 cents
+            // A gold coin is worth 10 cents
+
+            // welcome
+            Console.WriteLine("Welcome to Money Maker!");
+
+            // getting user input
+            Console.Write("Enter Number of Cents : ");
+            string input = Console.ReadLine();
+            double inputNum = Math.Floor(Convert.ToDouble(input));
+
+            // leading on the user
+            Console.WriteLine($"{inputNum} Cents Equals...");
+
+            // variable definitions
+            int gold = 10;
+            int silver = 5;
+
+            // getting max gold coins
+            int goldCoins = Convert.ToInt32(Math.Floor(inputNum / gold));
+
+            // getting remainder amount
+            double remaining = inputNum % gold;
+
+            // getting max gold coins
+            int silverCoins = Convert.ToInt32(Math.Floor(remaining / silver));
+
+            // getting remainder amount
+            double bronzeCoins = inputNum % silver;
+
+            // printing output
+            Console.WriteLine($"Gold Coins : {goldCoins}");
+            Console.WriteLine($"Silver Coins : {silverCoins}");
+            Console.WriteLine($"Bronze Coins : {bronzeCoins}");
+        }
+    }
+}
