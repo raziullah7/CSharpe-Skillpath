9367ab94309c3e06467e87e2e54d1605d34a55a7
Razi Ullah <raziullah913@gmail.com>
2023-02-12 20:37:55 +0500

    a program to show the use of methods as parameters, aynonmyous methods and lambda expressions

---
+++ Beginner CSharpe/Methods/Lambda Expressions/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AlternateExpressions
+{
+    class Program
+    {
+        // Method to be used as second argument
+        public static bool IsLong(string word)
+        {
+            return word.Length > 8;
+        }
+
+        static void Main(string[] args)
+        {
+            // Array to be used as first argument
+            string[] adjectives = { "rocky", "mountainous", "cosmic", "extraterrestrial" };
+
+            // Call Array.Find() and 
+            // Pass in the array and method as arguments
+            string firstLongAdjective = Array.Find(adjectives, IsLong);
+
+            // or just use the inline notation using aynonmyous method
+            string firstLongAdjective2 = Array.Find(adjectives, word => word.Length > 8);
+
+            Console.WriteLine($"The first long word is: {firstLongAdjective}.");
+        }
+    }
+}
